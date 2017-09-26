bde798ce013940c869df306cb66539c470763ad9
Steven Copeland <steven@thomasleecopeland.com>
2017-09-26 10:41:57 -0400

    adding js code

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
 
+var y = 0
+var sx = 0;
+var sy = 0;
+var hx = 500;
+var hy = 500;
+
+var shoot = function() {
+	ctx.fillRect(sx, sy, 10, 10)
+	ctx.fillRect(hx, hy, 10, 10)
+	sx += 5;
+	sy += 5;
+	hx -= 5;
+	hy -= 5;
+}
+
+document.onkeydown = function(e) {
+	e = e || window.event;
+	var key = e.which || e.keyCode;
+	if (key === 32) {
+		shoot();
+	}
+}
+
+setInterval(function() {
+	if (y < 247) {
+		ctx.clearRect(0, 0, 500, 500);
+		y += 0.2
+		ctx.fillRect(y, y, 30, 30);
+	}
+}, 10);
