9c39c35c8f6faa9a69816c5be1c0586fc50ad012
Steven Copeland <steven@thomasleecopeland.com>
2017-09-26 15:12:21 -0400

    add first draft shooter

--- script.js
+++ script.js
@@ -3,22 +3,25 @@ var ctx = canvas.getContext("2d");
 
 var x = 220;
 var y = 450;
-var sx = 45;
-var sy = 50;
+var sx = x + 0;
+var sy = y + -10;
 
 ctx.fillRect(x, y, 30, 30)
 
 var shoot = function() {
-	ctx.clearRect(sx, sy, 10, 10)
-	sx += 7;
-	sy += 7;
-	ctx.fillRect(sx, sy, 10, 10)
+	if (sy >= 472) {
+		var sy = y + -10;
+	} else {
+		setInterval(function() {
+			ctx.clearRect(x, sy, 10, 10)
+			sy -= 7;
+			ctx.fillRect(x, sy, 10, 10)
+		}, 10);
+	}
 }
 
 function moveLeft() {
-	if (x >= 502) {
-		x = 502;
-	} else if (x <= 0) {
+	if (x <= 0) {
 		x = 0;
 	}
 	ctx.clearRect(x, y, 30, 30);
@@ -27,6 +30,9 @@ function moveLeft() {
 }
 
 function moveRight() {
+	if (x >= 472) {
+		x = 472;
+	}
 	ctx.clearRect(x, y, 30, 30);
 	x += 5
 	ctx.fillRect(x, y, 30, 30);
