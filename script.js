3b28ac6fecfbb63228833bd462a85e5ca8b94be7
Steven Copeland <steven@thomasleecopeland.com>
2017-09-26 17:24:51 -0400

    adding first draft enemies

--- script.js
+++ script.js
@@ -5,22 +5,47 @@ var x = 220;
 var y = 450;
 var sx = x + 0;
 var sy = y + -10;
+var mx = 50;
+var my = 50;
+ctx.fillRect(mx, my, 30, 30);
+ctx.fillRect(mx + 60, my, 30, 30);
+ctx.fillRect(mx + 120, my, 30, 30);
+ctx.fillRect(mx + 180, my, 30, 30);
+ctx.fillRect(mx + 240, my, 30, 30);
+ctx.fillRect(mx + 300, my, 30, 30);
+ctx.fillRect(mx + 360, my, 30, 30);
 
-ctx.fillRect(x, y, 30, 30)
+ctx.fillRect(mx, my + 50, 30, 30);
+ctx.fillRect(mx + 60, my + 50, 30, 30);
+ctx.fillRect(mx + 120, my + 50, 30, 30);
+ctx.fillRect(mx + 180, my + 50, 30, 30);
+ctx.fillRect(mx + 240, my + 50, 30, 30);
+ctx.fillRect(mx + 300, my + 50, 30, 30);
+ctx.fillRect(mx + 360, my + 50, 30, 30);
+
+
+ctx.fillRect(x, y, 30, 30);
 
 var shoot = function() {
-	if (sy >= 500) {
-		sy = y + -10;
-	} else {
+	ctx.fillStyle = "yellow";
+	if (sy >= 0) {
 		setInterval(function() {
 			ctx.clearRect(x, sy, 10, 10)
 			sy -= 7;
-			ctx.fillRect(x, sy, 10, 10)
-		}, 10);
+			ctx.fillRect(x, sy, 10, 10);
+			// if (sy >= mx && sy >= my) {
+			// 	ctx.clearRect(mx, my, 30, 30);
+			// }
+		}, 10)
+	} else {
+		ctx.clearRect(x, sy, 10, 10)
+		sx = x + 0;
+		sy = y + -10;
 	}
 }
 
 function moveLeft() {
+	ctx.fillStyle = "black";
 	if (x <= 0) {
 		x = 0;
 	}
@@ -30,6 +55,7 @@ function moveLeft() {
 }
 
 function moveRight() {
+	ctx.fillStyle = "black";
 	if (x >= 472) {
 		x = 472;
 	}
