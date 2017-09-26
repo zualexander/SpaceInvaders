46c42748f9cc3dd39c07ffdd9237564b8b53d09a
Steven Copeland <steven@thomasleecopeland.com>
2017-09-26 14:46:49 -0400

    adding moving code

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
-var y = 0
-var sx = 0;
-var sy = 0;
-var hx = 500;
-var hy = 500;
+var x = 220;
+var y = 450;
+var sx = 45;
+var sy = 50;
+
+ctx.fillRect(x, y, 30, 30)
 
 var shoot = function() {
+	ctx.clearRect(sx, sy, 10, 10)
+	sx += 7;
+	sy += 7;
 	ctx.fillRect(sx, sy, 10, 10)
-	ctx.fillRect(hx, hy, 10, 10)
-	sx += 5;
-	sy += 5;
-	hx -= 5;
-	hy -= 5;
+}
+
+function moveLeft() {
+	if (x >= 502) {
+		x = 502;
+	} else if (x <= 0) {
+		x = 0;
+	}
+	ctx.clearRect(x, y, 30, 30);
+	x -= 5
+	ctx.fillRect(x, y, 30, 30);
+}
+
+function moveRight() {
+	ctx.clearRect(x, y, 30, 30);
+	x += 5
+	ctx.fillRect(x, y, 30, 30);
 }
 
 document.onkeydown = function(e) {
@@ -21,13 +37,18 @@ document.onkeydown = function(e) {
 	var key = e.which || e.keyCode;
 	if (key === 32) {
 		shoot();
+	} else if (key === 37) {
+		moveLeft();
+	} else if (key === 39) {
+		moveRight();
 	}
 }
 
-setInterval(function() {
-	if (y < 247) {
-		ctx.clearRect(0, 0, 500, 500);
-		y += 0.2
-		ctx.fillRect(y, y, 30, 30);
-	}
-}, 10);
+// var y = 0
+// setInterval(function() {
+// 	if (y < 50) {
+// 		ctx.clearRect(y, y, 30, 30);
+// 		y += 0.2
+// 		ctx.fillRect(y, y, 30, 30);
+// 	}
+// }, 10);
