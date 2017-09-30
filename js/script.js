36ecd26dd575ed50504f5668ae27422117904322
VRamazing <r.vignesh149@gmail.com>
2017-09-30 20:23:34 +0530

    Restructuring code and increasing variable usability

--- js/script.js
+++ js/script.js
@@ -2,12 +2,26 @@ var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
 // Global variables
-var x = 220;
-var y = 450;
-var sx = x;
-var sy = y - 10;
+
+
+//PLAYER
+const PLAYER_WIDTH = 20;
+const PLAYER_HEIGHT = 30;
+var x = canvas.width/2; //player poition x
+var y = canvas.height - PLAYER_HEIGHT ; //player position y
+
+//Bullet Position
+var sx = x   ; //XPoition of user bullet origin
+var sy = y - PLAYER_HEIGHT/2 ;//YPosition of user bullet origin.
+const BULLET_SIZE = 10;
+
+
+//Monster
+const MONSTER_HEIGHT =30;
+const MONSTER_WIDTH = 30;
 var mx = 50;
 var my = 50;
+
 
 // Draw monster squares
 // TODO: Minify code
@@ -28,52 +42,56 @@ ctx.fillRect(mx + 240, my + 50, 30, 30);
 ctx.fillRect(mx + 300, my + 50, 30, 30);
 ctx.fillRect(mx + 360, my + 50, 30, 30);
 
+
+
+
+
 // Player square
-ctx.fillRect(x, y, 30, 30);
+ctx.fillRect(x, y, PLAYER_WIDTH, PLAYER_HEIGHT);
 
 var bulletMovement;
 
 // Shooting function for the player
 var shoot = function() {
+
 	if (sy >= 0) { // Checks if the bullet is passed the end of the screen.
 		if (bulletMovement) return;
 
 		bulletMovement = setInterval(function() {
-			ctx.clearRect(sx, sy, 10, 10);
-
-			sy -= 10;
-
+			ctx.clearRect(sx, sy, BULLET_SIZE, BULLET_SIZE);
+			sy -= BULLET_SIZE;
 			ctx.fillStyle = "yellow";
-			ctx.fillRect(sx, sy, 10, 10);
+			ctx.fillRect(sx , sy, BULLET_SIZE, BULLET_SIZE);
 		}, 10)
 	} else { // if the bullets are passed the end of the screen, set the position to normal and erase the square.
-		ctx.clearRect(sx, sy, 10, 10)
-		sx = x + 0;
-		sy = y - 10;
+		ctx.clearRect(sx, sy, BULLET_SIZE, BULLET_SIZE)
+		sx = x  ; //XPoition of user bullet origin
+		sy = y - PLAYER_HEIGHT/2 ;//YPosition of user bullet origin.
 	}
 }
 
 // This function is called when moving left(player)
 function moveLeft() {
 	ctx.fillStyle = "black";
-	if (x <= 0) { // Collision detection on the left side
-		x = 0;
-	}
-	ctx.clearRect(x, y, 30, 30);
-	x -= 5
-	ctx.fillRect(x, y, 30, 30);
+	if (x >= 2) { // Collision detection on the left side
+		ctx.clearRect(x, y, PLAYER_WIDTH, PLAYER_HEIGHT);
+		x -= 5
+		ctx.fillRect(x, y, PLAYER_WIDTH, PLAYER_HEIGHT);
+	}	
 }
 
 // Function for moving right(player)
 function moveRight() {
 	ctx.fillStyle = "black";
-	if (x >= 472) { // Collision detection on the right side
-		x = 472;
+	if (x <= 475) { // Collision detection on the right side
+		ctx.clearRect(x, y, PLAYER_WIDTH, PLAYER_HEIGHT);
+		x += 5
+		ctx.fillRect(x, y, PLAYER_WIDTH, PLAYER_HEIGHT);
 	}
-	ctx.clearRect(x, y, 30, 30);
-	x += 5
-	ctx.fillRect(x, y, 30, 30);
+	
 }
+
+
 
 // Adds key effects for moving and shooting
 document.onkeydown = function(e) {
